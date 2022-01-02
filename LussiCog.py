f35e75a2d54d94dd917fd7e9aa1eba1ce09a2998
v-boissonneault <v.boissonneault@gmail.com>
2022-01-02 18:42:44 -0500

    cleanup de musicbot.py

--- LussiCog.py
+++ LussiCog.py
@@ -10,3 +10,22 @@ class LussiCog(commands.Cog):
     @commands.command(name='helloL', description="")
     async def hello_(self, ctx):
         await ctx.send("Hello from LussiCog")
+
+    @commands.command(name='tony2')
+    async def kek_(self, ctx):
+        await ctx.send(''':salty:''')
+    
+    
+    tonySel = False
+    @commands.command(name='sel')
+    async def sel_(self, ctx):
+        if (self.tonySel == True):
+            self.tonySel = False
+        elif (self.tonySel == False):
+            self.tonySel = True
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        print(message.author.id)
+        if (message.author.id == 172835127016030208 and self.tonySel == True):
+            await message.add_reaction("🧂")  
--- musicbot.py
+++ musicbot.py
@@ -468,25 +468,4 @@ class MusicCog(commands.Cog):
         await ctx.send('**Successfully disconnected**')
 
         await self.cleanup(ctx.guild)
-     
-    @commands.command(name='tony2')
-    async def kek_(self, ctx):
-        await ctx.send(''':salty:''')
     
-    
-    tonySel = False
-    @commands.command(name='sel')
-    async def sel_(self, ctx):
-        if (self.tonySel == True):
-            self.tonySel = False
-        elif (self.tonySel == False):
-            self.tonySel = True
-    
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        print(message.author.id)
-        if (message.author.id == 172835127016030208 and self.tonySel == True):
-            await message.add_reaction("🧂")  
-
-
-
