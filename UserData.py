a2a9ca88a9817b2a8b4115d144bfb9637616373d
Etienne Richan <rice1902@usherbrooke.ca>
2022-01-02 12:04:36 -0500

    Add stinger files + userdata file

---
+++ UserData.py
@@ -0,0 +1,23 @@
+
+from enum import Enum
+
+class NameID(Enum):
+    Etienne = 168154774041001995
+    Alexis = 172826049099333632
+    Benne = 172831943119994881
+    Tony = 172835127016030208
+    Vince = 172850936379736064
+    Math = 175989268609499136
+    Gab = 587822829945356310
+    Oli = 690719015441793045
+
+StingerMap = {
+    NameID.Etienne : "Etienne.m4a",
+    NameID.Alexis : "Alexis.m4a",
+    NameID.Benne : "Benne.m4a",
+    NameID.Tony : "Tony.m4a",
+    NameID.Math : "Math.m4a",
+    NameID.Gab : "Gab.m4a",
+    NameID.Oli : "Oli.m4a",
+
+}
