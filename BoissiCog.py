3adf4af42f635b45caac5f78a487303d3ff2c2e8
v-boissonneault <v.boissonneault@gmail.com>
2022-01-04 19:56:39 -0500

    Added games commands + SQLite DB (scuffed edition)

--- BoissiCog.py
+++ BoissiCog.py
@@ -4,12 +4,21 @@ import UserData
 import asyncio
 import os
 import random as rand
+import sqlite3 as db
+from enum import Enum, auto
 
+#MESSAGES
+NOS_OPTIONS = "Voici nos options:"
+PAS_OPTIONS = "On a pas d'options!"
+ERR_GENERIC = "Oops! Ç'a pas marché!"
+
+#HELP MESSAGES
+HELP_ADDGAME = "Usage: !addgame [Game Name] [Max. Players]"
 
 #PATHS
 PIZZA_IMG = os.path.abspath("res/pizza/img/")
 PIZZA_SND = os.path.abspath("res/pizza/snd/")
-
+DB = "res/bot.db"
 
 class BoissiCog(commands.Cog):
     
@@ -31,6 +40,47 @@ class BoissiCog(commands.Cog):
         player = AudioPlayer(self.bot, ctx.channel.guild)
         await ctx.send("It's pizza time!", file=discord.File(self.get_rand_file(PIZZA_IMG)))
         await player.play(self.get_rand_file(PIZZA_SND))
+
+    @commands.command(name='addgame', description='Add a game to the database')
+    async def add_game_(self, ctx, *, arg=None):
+        try:
+            parts = arg.split(" ")
+            l = len(parts)
+            maxp = parts.pop(l-1)
+            name = ' '.join(parts)
+
+            if maxp and name:
+                con = db.connect(DB)
+                cur = con.cursor()
+                cur.execute("CREATE TABLE IF NOT EXISTS Games (name text, max_players integer)")
+                cur.execute("INSERT INTO Games VALUES (?,?)", (name, maxp))
+                con.commit()
+                con.close()
+                await ctx.send("'%s' was added to the game list" % name)
+            else:
+                await ctx.send(HELP_ADDGAME)
+        except:
+            await ctx.send(HELP_ADDGAME)
+
+    @commands.command(name='gamelist', aliases=['options', 'jeux', 'games'], description='List of games to play with the bois')
+    async def list_games_(self, ctx, numpl=0):
+        try:
+            con = db.connect(DB)
+            cur = con.cursor()
+            if numpl:
+                q = cur.execute("SELECT * FROM Games WHERE max_players >= ?", (numpl,)).fetchall()
+            else:
+                q = cur.execute("SELECT * FROM Games").fetchall()
+            await ctx.send(NOS_OPTIONS)
+            if q:
+                i = 0
+                for game in q:
+                    i += 1
+                    await ctx.send("%i. %s pour %i kekeurs" % (i, game[0], game[1]))
+            else:
+                await ctx.send(PAS_OPTIONS)
+        except:
+            await ctx.send(ERR_GENERIC)
 
 
 class AudioPlayer :
